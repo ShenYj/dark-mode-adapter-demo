78a6072a07565fd364c7febe3d1a021d4961db10
ShenYj <shenyanjie123@foxmail.com>
2020-07-11 20:42:38 +0800

    add.

--- DarkModeDemo/DarkModeDemo/DarkModeAdapterDemo/Home/HomeViewController.swift
+++ DarkModeDemo/DarkModeDemo/DarkModeAdapterDemo/Home/HomeViewController.swift
@@ -13,7 +13,11 @@ class HomeViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if #available(iOS 13.0, *) {
+            view.backgroundColor = UIColor.systemFill
+        } else {
+            // Fallback on earlier versions
+        }
     }
     
-
 }
