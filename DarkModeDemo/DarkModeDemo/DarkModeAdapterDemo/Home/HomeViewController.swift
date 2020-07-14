9a87a13b26669055a587309ff0e4acde94e70f5a
ShenYj <shenyanjie123@foxmail.com>
2020-07-14 11:53:59 +0800

    rebase test develop 3: remove space

--- DarkModeDemo/DarkModeDemo/DarkModeAdapterDemo/Home/HomeViewController.swift
+++ DarkModeDemo/DarkModeDemo/DarkModeAdapterDemo/Home/HomeViewController.swift
@@ -13,9 +13,7 @@ class HomeViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
     }
-    
 }
 
 extension HomeViewController {
--- DarkModeDemo/DarkModeDemo/DarkModeAdapterDemo/Setting/SettingViewController.swift
+++ DarkModeDemo/DarkModeDemo/DarkModeAdapterDemo/Setting/SettingViewController.swift
@@ -14,7 +14,6 @@ class SettingViewController: BaseViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 }
 
 extension SettingViewController {
